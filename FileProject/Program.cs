9b9ef6b1e1a441a2aeb88feebb3bfbd2017a5842
ליאור מסטורוב <liormasturov@hotmail.com>
2023-05-30 14:04:03 +0300

    added isnumber and isletter

--- FileProject/Program.cs
+++ FileProject/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace FileProject
 {
@@ -10,7 +12,19 @@ namespace FileProject
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader(@"./input.txt");
 
+        }
+
+        public static bool IsNumber(string str)
+        {
+            Regex regex = new Regex("^[0-9]+$", RegexOptions.IgnoreCase);
+            return regex.IsMatch(str);
+        }
+        public static bool IsLetter(string str)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$", RegexOptions.IgnoreCase);
+            return regex.IsMatch(str);
         }
     }
 }
